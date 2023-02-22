f06b40b4a3f2d208626973b3181d9cd5d9dca4b9
sophiacarlone <sophiamcar@gmail.com>
2023-02-22 14:16:36 -0500

    inital commit for p/c

---
+++ ConsumerProducer/producer_consumer.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
+
+/* Global variables and Defines */
+int lock = 0;
+
+/* Functions */
+void *Producer();
+void *Consumer();
+
+int main(){
+pthread_t producer, consumer;
+
+
+}
+
+void *Producer(){
+
+}
+
+void *Consumer(){
+    
+}
