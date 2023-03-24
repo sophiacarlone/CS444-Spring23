e950b0809bd51bc17431054e9b8362ff06811e07
sophiacarlone <sophiamcar@gmail.com>
2023-03-24 16:23:56 -0400

    simple notes and setup for hw3

--- Keylogger/main.cpp
+++ Keylogger/main.cpp
@@ -5,3 +5,39 @@
 // Description
 // Date
 // Video link
+
+/*
+1. creating a deamon process in c/c++
+2. keylogging to a string
+2 ways:
+    A. Reading keyboard events. On linux, reading them from /dev/input/eventX. (ie. event0)
+find correct index. root privilage?
+    B. Taking user input directly from stdin. (getchar(), etc.)
+3. writing the string in step 2 to a file.
+
+
+*/
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(){
+    ofstream out;
+    string filename = "keys_logged.txt";
+    
+    //this is the file that is logging all the characters. Does it need more information?
+    out.open(filename);
+
+    /*dont need to use threads because people can really only really give one keyboard input at a time
+    *even if you try to press at the same time there is a slight difference. */
+    char c; //character (do it one at a time or can I do something more efficient)
+
+    while(true){
+        //getting keys
+        c = getchar();
+        if( c == '}') break;
+        out << c;
+    }
+}
