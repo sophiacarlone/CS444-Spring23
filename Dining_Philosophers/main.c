8143e2929d1d99bef4aeed62505ca1a96532f66c
sophiacarlone <sophiamcar@gmail.com>
2023-04-28 18:41:00 -0400

    break

--- Dining_Philosophers/main.c
+++ Dining_Philosophers/main.c
@@ -28,6 +28,8 @@ typedef struct philosopher_info
 //global variables
 pthread_mutex_t chopsticks_in_use[CHOPSTICKS]; //array to symbolize which and how many forks are in use
 philosopher_info info[PHILOSOPHERS]; //information for each philosopher
+pthread_cond_t chopstick_cond[CHOPSTICKS];
+// time_t alarm;
 
 //functions
 void *PhilosopherActions(void *args);
@@ -37,11 +39,13 @@ void print_info(int id, int state);
 int main(){
     int status; // pthread creation check variable
     srand(time(0)); //Seed for random status fill
+    // time(alarm);
 
     pthread_t philosophers[PHILOSOPHERS]; //each philosopher has its own thread
 
     for(int i = 0; i < CHOPSTICKS; i++){
         pthread_mutex_init(&chopsticks_in_use[i], 0); //initialize the mutex with default attribute
+        pthread_cond_init(&chopstick_cond[i], 0);
     }
 
     for(int i = 0; i < PHILOSOPHERS; i++){
@@ -52,8 +56,11 @@ int main(){
             fprintf(stderr, "issue with thread\n");
             exit(1); //checking all threads are fine
         }
-        sleep(2); //used to avoid conflict when using fprintf()
     }
+    for (int i = 0; i < PHILOSOPHERS; i++){
+        pthread_join( philosophers[i], NULL);
+    }
+    
 }
 
 //TODO: pthread conditions
@@ -77,39 +84,51 @@ void *PhilosopherActions(void *args){
     int owns_left_chopsticks = 0;
     int owns_right_chopsticks = 0;
 
-    fprintf(stderr, "hit1\n");
-
     while(1){
-    fprintf(stderr, "hit2\n");
-
         switch (data->state){
         case FULL:
-            fprintf(stderr, "hit3\n");
             pthread_mutex_unlock(&chopsticks_in_use[left_chopstick]); //letting go of the chopstick
             pthread_mutex_unlock(&chopsticks_in_use[right_chopstick]); //letting go of the chopstick
+            // pthread_cond_signal(&chopstick_cond[left_chopstick]);
+            // pthread_cond_signal(&chopstick_cond[right_chopstick]);
             data->state -= 1; //will be getting hungry
             print_info(data->id, FULL);
+
+            // fprintf(stderr, "hit3\n"); //confirmed good
             sleep(10); //wait for cycle to be over
-            break;
+            // break;
         
         case HUNGRY:
-            fprintf(stderr, "hit4\n");
             //check left
+            // fprintf(stderr, "hit4\n");
+
             if( (info[left_partner].state == STARVING) ){
                 pthread_mutex_unlock(&chopsticks_in_use[left_chopstick]); //letting go of the chopstick
+                // pthread_cond_signal(&chopstick_cond[left_chopstick]);
                 owns_left_chopsticks = 0;
             }
             else{
+                // fprintf(stderr, "hit5\n");
+
                 pthread_mutex_lock(&chopsticks_in_use[left_chopstick]); //picking up chopstick
+                // fprintf(stderr, "hit6\n");
+
+                // pthread_cond_wait(&chopstick_cond[left_chopstick], &chopsticks_in_use[left_chopstick]); 
+                // fprintf(stderr, "hit7\n");
+
                 owns_left_chopsticks = 1;
+
             }
             //check right
+
             if(info[right_partner].state == STARVING){
                 pthread_mutex_unlock(&chopsticks_in_use[right_chopstick]); //letting go of the chopstick
+                // pthread_cond_signal(&chopstick_cond[right_chopstick]);
                 owns_right_chopsticks = 0;
             }
             else{
                 pthread_mutex_lock(&chopsticks_in_use[left_chopstick]); //picking up chopstick
+                // pthread_cond_wait(&chopstick_cond[right_chopstick], &chopsticks_in_use[right_chopstick]); 
                 owns_right_chopsticks = 1;
             }
 
@@ -121,19 +140,22 @@ void *PhilosopherActions(void *args){
             print_info(data->id, HUNGRY);
 
             sleep(10);
-            break;
+            // break;
 
         case STARVING:
+            fprintf(stderr, "starving\n");
             pthread_mutex_lock(&chopsticks_in_use[left_chopstick]); //get chopstick
             pthread_mutex_lock(&chopsticks_in_use[right_chopstick]); //get chopstick
+            // pthread_cond_wait(&chopstick_cond[left_chopstick], &chopsticks_in_use[left_chopstick]); 
+            // pthread_cond_wait(&chopstick_cond[right_chopstick], &chopsticks_in_use[right_chopstick]); 
             data->state += 1;
             sleep(10);
-            break;
+            // break;
 
         case DEAD:
             fprintf(stderr, "GAME OVER");
             exit(1);
-            break;
+            // break;
 
         default:
             fprintf(stderr, "fell to default");
@@ -148,51 +170,3 @@ void *PhilosopherActions(void *args){
 void print_info(int id, int state){
     fprintf(stderr, "Philosopher %d is in state %d\n", id, state);
 }
-
-
-
-
-
-
-
-/*~~~~~~~~~~~~~~~~~~~~~~~~GARBAGE~~~~~~~~~~~~~~~~~~~~~~~~*/
-
-        
-        // if (data->state == FULL){
-        //     pthread_mutex_unlock(&chopsticks_in_use[left_chopstick]); //letting go of the buffer
-        //     pthread_mutex_unlock(&chopsticks_in_use[right_chopstick]); //letting go of the buffer
-        //     sleep(10);
-        // }
-        //else state - 1
-
-
-/* RULES
-
-You must have 5 philosophers and 5 forks on the table
-Each fork is shared between two adjacent philosophers (critical regions)
-Only one philosopher can use a fork, and multiple can be eating at the same time
-Philosophers have four states
-    Thinking: hunger status is full and the philosopher is idle thinking, not eating
-    Eating: hunger status is not full and triggers philosopher to eat until thinking can resume
-    Hungry: Philsopher can no longer think due to junger and needs to commence eating before starving
-    Dead: philosopher is in the great beyond
-
-Each philosopher should have a hunger meter tracking how close they are to death
-    cli status updates
-    timer based. Each cycle in which a philosopher doesnt eat causes more hunger. decide on how many cycles before hunger
-
-No deadlocks. Can ideally go on forever
-
-*/
-
-/* ideas:
-
-You can assume that every philosopher comes to the table full, but then things will turn sour
-Instead, randomize each philosopher at the beginning to have a state of 3 or 4 (Eating or Thinking)
-Odd fork out should be randomly place to someone who is not eating
-Move forks separately
-
-When someone is full, they should not need the fork
-When someone is at 2, they should get the highest priority
-
-*/
